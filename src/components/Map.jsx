79d0c972eb53287ac05f759b6ddb9e55dd6f6eaa
rohit0796 <91247476+rohit0796@users.noreply.github.com>
2023-06-10 16:37:26 +0530

    map zoomed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -37,7 +37,7 @@ const Map = ({latitude,longitude,term}) => {
 
   return (
     <div className='map-leaflet' style={{width:`100%`}}>
-      <MapContainer center={coord} zoom={term?3:10} scrollWheelZoom={false} style={{ height: '100%' }}>
+      <MapContainer center={coord} zoom={term?3:15} scrollWheelZoom={false} style={{ height: '100%' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=rso8CExorIToPrWld3QF"
