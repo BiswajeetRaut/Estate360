eeda8b2eb4c699c7867a39f98b1da1be868cd229
Biswajeet Raut <85820671+BiswajeetRaut@users.noreply.github.com>
2023-06-15 15:23:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyBTxqGuG0PTOrAizot3xjQ1oLx9K31P3Do",
-  authDomain: "estate360-521e7.firebaseapp.com",
-  projectId: "estate360-521e7",
-  storageBucket: "estate360-521e7.appspot.com",
-  messagingSenderId: "985805184126",
-  appId: "1:985805184126:web:70c5feb1aacd2f25164c2f",
-  measurementId: "G-ENQ063CKKY"
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db=firebaseApp.firestore();
